c1a8854efffc5f76d9b15b38ba7a79f8a0052251
Mayank Mandava <mayank.mandava@afterpay.com>
2019-10-07 11:52:14 -0700

    Small changes

--- public/client.js
+++ public/client.js
@@ -10,69 +10,66 @@ class Moves {
   constructor() {
     const seed = inRange(Math.random(), 1, 10000);
     this.setSeed(seed);
-    
+
     nextBtn.onclick = () => this.next();
     prevBtn.onclick = () => this.prev();
     resetBtn.onclick = () => this.reset();
     setBtn.onclick = () => {
       this.setSeed(seedInp.value);
       this.render();
-    }
-    
+    };
   }
   next() {
-    if(this.current < this.count - 1)
-      this.current++;
+    if (this.current < this.count - 1) this.current++;
     this.render();
   }
   prev() {
-    if(this.current > 0)
-      this.current--;
+    if (this.current > 0) this.current--;
     this.render();
   }
   reset() {
     this.current = 0;
     this.render();
   }
-  
+
   setSeed(seed) {
     seedInp.value = seed;
     this.rndGen = mulberry32(seed);
-    
-    this.moves = []
-    for(let num = 1; num <= 10; num++) {
-      for (let move of ["pushes", "pulls", "raises", "squats"] ) {
-        this.moves.push(`${num} ${move}`)
+
+    this.moves = [];
+    for (let num = 1; num <= 10; num++) {
+      for (let move of ["pushes", "pulls", "raises", "squats"]) {
+        this.moves.push(`${num} ${move}`);
       }
     }
-    for(let i = 0; i < this.moves.length - 1; i ++) {
-      const j = inRange(this.rndGen(), i+1, this.moves.length - 1);
+    for (let i = 0; i < this.moves.length - 1; i++) {
+      const j = inRange(this.rndGen(), i + 1, this.moves.length - 1);
       const temp = this.moves[i];
       this.moves[i] = this.moves[j];
       this.moves[j] = temp;
     }
+    this.moves.push("All done!");
     this.count = this.moves.length;
-    this.current = 0
+    this.current = 0;
   }
-  
+
   render() {
-    countDiv.innerHTML = `${this.current+1}/${this.count}`;
+    countDiv.innerHTML = `${this.current + 1}/${this.count}`;
     currentDiv.innerHTML = this.moves[this.current];
   }
 }
 
-
 function mulberry32(a) {
-    return function() {
-      var t = a += 0x6D2B79F5;
-      t = Math.imul(t ^ t >>> 15, t | 1);
-      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-      return ((t ^ t >>> 14) >>> 0) / 4294967296;
-    }
+  return function() {
+    var t = (a += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
 }
 
 function inRange(a, min, max) {
-  return Math.floor(a * (max - min + 1)) + min; 
+  return Math.floor(a * (max - min + 1)) + min;
 }
 
 const moves = new Moves();
